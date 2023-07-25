414ad566812af874f882067ed393b1b71f933d92
gokulakrishnangv <140329576+gokulakrishnangv@users.noreply.github.com>
2023-07-25 17:22:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title("My Parents New Healthy Dinner")
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
